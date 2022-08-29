3b23d806d7aeb043ae25e9d9e0f9d917398841bb
Isaiah Magana <Maganaisaiah4@gmail.com>
2022-08-29 17:36:33 -0400

    Create one of the text field widgets for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,6 +11,52 @@ class MyApp extends Statefulwidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
         ),
+        home: TempApp(),
     );
   }
 }
+
+class TempApp extends StatefulWidget {
+  @override
+  TempState createState() => TempState();
+}
+
+class TempState extends State<TempApp> {
+  double input;
+  double output;
+  bool fOrC;
+
+  @override
+  void initState() {
+    super.initState();
+    input = 0.0;
+    output = 0.0;
+    fOrC = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextField inputField = TextField(
+      keyboardType: TextInputType.number, 
+      onChanged: (str){     //Run everytime user trys to type in a string instead of a number
+        try {
+          input = double.parse(str);
+        } catch(e) {
+          input = 0.0;    //Input set if the user dose use a string over a number
+        }
+      },
+      decoration: 
+    );
+
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
+
+
+
